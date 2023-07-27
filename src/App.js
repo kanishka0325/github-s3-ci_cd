36a00bf8c32407005e144c52ec9e98e58e17285c
kanishka0325 <purna@web3genes.com>
2023-07-27 14:30:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Github S3 CodeDeploy React Application.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+          <span style={{ fontSize:'12px' }}>Version: 1.0</span>
+     
       </header>
     </div>
   );
